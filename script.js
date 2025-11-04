0c1fcf62c887c4efc7ca77b957f4be8c30324068
sistowant <suittowan@gmail.com>
2025-11-05 00:26:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const nextBtn = document.querySelector(".forward");
 
 // the image array
 const images = [
-  "./img/01-living-room-blue-theme.jpg",
+  "https://www.dropbox.com/scl/fi/eyytymevh4b1a99xl2smd/1.jpg?rlkey=7wqqitok7i381iw5vmmykc5l4&st=zf39ssv4&dl=0",
   "./img/02-living-room-couch.jpg",
   "./img/03-living-room-red-theme.jpg",
   "./img/04-modern-kitchen.jpg",
