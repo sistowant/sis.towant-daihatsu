1165722b12b66157fa18754e4392ca44e16b8507
sistowant <suittowan@gmail.com>
2026-03-24 23:47:17 +0700

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,20 +1,28 @@
 var slider_img = document.querySelector('.slider-img');
 var Img = ['1.jpg', '2.jpg', '3.jpg', 'e.jpg'];
+var pickup = ['4.jpg', '5.jpg', '6.jpg', 'e.jpg'];
 var i = 0;
 
 function prev(){
-	if(i <= 0) i = Img.length;	
+	if(i <= 0) 
+		i = Img.length;	
+	    i = pickup.length;
 	i--;
 	return setImg();			 
 }
 
 function next(){
 	if(i >= Img.length-1) i = -1;
+	if(i >= pickup.length-1) i = -1;
 	i++;
 	return setImg();			 
 }
 
 function setImg(){
 	return slider_img.setAttribute('src', "Img/"+Img[i]);
+	
+}
+function setpickup(){
+	return slider_img.setAttribute('src', "pickup/"+pickuo[i]);
 	
 }
