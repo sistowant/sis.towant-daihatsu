e9e0e97a25c854ffe7dd515fa20f343a1b75fd13
sistowant <suittowan@gmail.com>
2026-03-27 14:23:31 +0700

    Update Slider.js

--- Slider.js
+++ Slider.js
@@ -1,20 +1,20 @@
 var slider_img = document.querySelector('.slider-img');
-var Img = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg'];
+var Img = ['6.jpg', '5.jpg', '4.jpg', '3.jpg', '2.jpg', '1.jpg'];
 var i = 0;
 
 function prev(){
-	if(i <= 0) i = Img.length;	
+	if(i <= 0) i = img.length;	
 	i--;
-	return setImg();			 
+	return setimg();			 
 }
 
 function next(){
-	if(i >= Img.length-1) i = -1;
+	if(i >= img.length-1) i = -1;
 	i++;
-	return setImg();			 
+	return setimg();			 
 }
 
-function setImg(){
+function setimg(){
 	return slider_img.setAttribute('src', "Img/"+Img[i]);
 	
 }
