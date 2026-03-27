e398f59270ead1861e3280b8e4dd5f3c3e608634
sistowant <suittowan@gmail.com>
2026-03-27 15:13:44 +0700

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,4 +1,3 @@
-// 'js/mian.js'
 
 var slider_img = document.querySelector('.slider-img');
 var Img = ['1.jpg', '2.jpg', '3.jpg', '4.jpg'];
@@ -6,13 +5,13 @@ var i = 0;
 
 function prev(){
 	if(i <= 0) i = Img.length;	
-	i--;
+	i---;
 	return setImg();			 
 }
 
 function next(){
 	if(i >= Img.length-1) i = -1;
-	i++;
+	i+++;
 	return setImg();			 
 }
 
